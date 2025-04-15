71cc5831ab351cda8185b1bb1aed6de93ea6de52
IJAHARKHAN <khan7oct@gmail.com>
2025-04-15 22:19:07 +0530

    Get latest

--- textutils/src/Components/TextForm.js
+++ textutils/src/Components/TextForm.js
@@ -52,17 +52,18 @@ function TextForm(props) {
         <Alert alert={alert} alertMessage={alertMessage}/>
 
         <div className="container">
-        <div className="my-2">
+        <div className="my-1">
         <h4 htmlFor="exampleFormControlTextarea1" className="form-label">{props.formText}</h4>
-        <textarea className="form-control" style={{ backgroundColor: props.mode === 'light' ? 'white' : 'black', color: props.mode === 'light' ? 'black' : 'white' }} id="exampleFormControlTextarea1" rows="5" value={text} onChange={(e) => {setText(e.target.value)}} placeholder='Enter Text..'></textarea>
+        <textarea className="form-control textarea" style={{ backgroundColor: props.mode === 'light' ? 'white' : 'black', color: props.mode === 'light' ? 'black' : 'white' }} id="exampleFormControlTextarea1" rows="5" value={text} onChange={(e) => {setText(e.target.value)}} placeholder='Enter Text..'></textarea>
         </div>
-        <button className="btn btn-primary btn-sm me-2" onClick={handleUpperCase}>Convert To Upper Case</button>
-        <button className="btn btn-primary btn-sm me-2" onClick={handleLowerCase}>Convert To Lower Case</button>
-        <button className="btn btn-primary btn-sm me-2" onClick={handleCopy}>Copy Text</button>
-        <button className="btn btn-primary btn-sm me-2" onClick={handleRemoveExtraSpaces}>Remove Extra Spaces</button>
-        <button className="btn btn-primary btn-sm me-2" onClick={handleClear}>Clear</button>
+        <button className="btn btn-primary btn-sm my-1 me-1" onClick={handleUpperCase}>Convert To Upper Case</button>
+        <button className="btn btn-primary btn-sm my-1 me-1" onClick={handleLowerCase}>Convert To Lower Case</button>
+        <button className="btn btn-primary btn-sm my-1 me-1" onClick={handleCopy}>Copy Text</button>
+        <button className="btn btn-primary btn-sm my-1 me-1" onClick={handleRemoveExtraSpaces}>Remove Extra Spaces</button>
+        <button className="btn btn-primary btn-sm my-1 me-1" onClick={handleClear}>Clear</button>
         <h5 className='my-2'>Your Text Summary</h5>
-        <p>{ text.length > 0 ? text.split(' ').length : '0'  } Words, {text.length} Characters</p>       
+        {/* <p>{ text.length > 0 ? text.split(' ').length : '0'  } Words, {text.length} Characters</p>   */}
+        <p>{ text.split(' ').filter((item)=>(item.length!==0)).length   } Words, {text.length} Characters</p>  
         <p>{0.008 * text.split(' ').length} Minutes Read</p>
         <h2>Preview</h2>
         <div>{text.length > 0 ? text : 'Enter text in text area.'}</div>
